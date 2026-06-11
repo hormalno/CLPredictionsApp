d2783a7398284b4ebc59b51238c97c67d8f5f8c4
hormalno <ivanov.yasen@gmail.com>
2026-06-12 01:11:34 +0300

    your message

--- backend/predictions/admin.py
+++ backend/predictions/admin.py
@@ -8,7 +8,9 @@ class MatchPredictionAdmin(admin.ModelAdmin):
 
 @admin.register(KnockoutPrediction)
 class KnockoutPredictionAdmin(admin.ModelAdmin):
-    list_display = ('match', 'predicted_home_team', 'predicted_away_team', 'predicted_winner', 'points')
+    list_display = ('user', 'match', 'predicted_home_team', 'predicted_away_team', 'predicted_winner', 'points')
+    list_filter = ('user',)
+    search_fields = ('user__username',)
 
 @admin.register(TopScorerPrediction)
 class TopScorerPredictionAdmin(admin.ModelAdmin):
