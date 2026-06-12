ffa28182353fb6781bc8256257d12a372643f4f2
hormalno <ivanov.yasen@gmail.com>
2026-06-12 21:05:12 +0300

    players admin

--- backend/players/admin.py
+++ backend/players/admin.py
@@ -5,5 +5,6 @@ from players.models import Player
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('name','team','jersey_number','position')
+    list_filter = ('team',)
     exclude = ('country_name',)
     ordering = ('id',)
